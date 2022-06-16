4c216871543e2d23430a57a768f00ff591f314b8
HolyJack <holyjackdev@gmail.com>
2022-06-16 19:55:58 +0300

    revert some changes

---
+++ src/executor/findpath.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   findpath.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ejafer <ejafer@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 19:05:03 by ejafer            #+#    #+#             */
+/*   Updated: 2022/06/14 19:13:04 by ejafer           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "executor.h"
+
+char	*find_path(t_mini *mini, char *name)
+{
+	char	**paths;
+	char	*path;
+	int		i;
+	char	*part_path;
+
+	i = 0;
+	while (ft_strncmp(mini->env[i], "PATH", 4))
+		i++;
+	paths = ft_split(mini->env[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, name);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		free(path);
+		i++;
+	}
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	free(paths);
+	return (NULL);
+}
