0ab37c68d88b1ad764842337c72e618740679158
dgizzard <dmitriypopov1543@gmail.com>
2022-06-30 18:24:00 +0300

    export trouble

--- src/builtins/mini_export.c
+++ src/builtins/mini_export.c
@@ -18,11 +18,8 @@
 char **add_var(char **env, int len, char *arg)
 {
 	int i;
-	int j;
 	char **new_env;
 
-	(void)j;
-	(void)arg;
 	new_env = malloc(sizeof(char *) * (len + 1));
 	i = -1;
 	while (env[++i])
@@ -31,31 +28,52 @@ char **add_var(char **env, int len, char *arg)
 	return (new_env);
 }
 
-char	**mini_export(t_command *cmd, char **env)
+int isvalid_arg(char *arg)
+{
+	if (!(ft_isalpha(arg[0])))
+	{
+		g_status = 1;
+		return (1);
+	}
+	if (!(ft_strchr(arg, '=')) || getcwd(arg, sizeof(arg)))
+	{
+		g_status = 0;
+		return (0);
+	}
+	g_status = 0;
+	return (2);
+
+}
+
+char	**export_var(char *arg, char **env)
 {
 	char 	**new_env;
 	int 	env_len;
 
 	env_len = 0;
-	if (!(cmd->argv[1]))
-	{
-		g_status = 1;
-		return (env);
-	}
-	if (!(ft_strchr(cmd->argv[1], '=')))
-	{
-		g_status = 0;
-		return (env);
-	}
-	if (!(ft_isalpha(cmd->argv[1][0])))
+	if (!arg)
 	{
 		g_status = 1;
 		return (env);
 	}
 	while (env[env_len])
 		env_len++;
-	new_env = add_var(env, env_len, cmd->argv[1]);
+//	if (isvalid_arg(arg) == 2)
+//		new_env = add_var(env, env_len, arg);
+//	else if (isvalid_arg(arg) == 0 || isvalid_arg(arg) == 1)
+//		return (env);
+	new_env = add_var(env, env_len, arg);
+
 	free_env(env);
-	g_status = 0;
 	return (new_env);
+}
+
+char	**mini_export(t_command *cmd, char **env)
+{
+	int		i;
+
+	i = 0;
+	while (cmd->argv[++i])
+		env = export_var(cmd->argv[i], env);
+	return (env);
 }
